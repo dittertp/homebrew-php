814307772c0df3caf2885ba613e010b3d8dc9882
Philipp Dittert <p.dittert@techdivision.com>
2026-01-21 08:37:42 +0100

    fix style

--- Formula/vsh-geoip.rb
+++ Formula/vsh-geoip.rb
@@ -7,13 +7,13 @@ class VshGeoip < Formula
   license "LGPL-2.1-or-later"
   head "https://github.com/maxmind/geoip-api-c.git", branch: "main"
 
-  conflicts_with "geoip",
-    because: "geoip has been disabled"
-
   bottle do
     root_url "https://ghcr.io/v2/dittertp/php"
     sha256 arm64_tahoe: "4cce6b57f254b8f145982591007091cf8eea6337baa95a65256d42a71172f96e"
   end
+
+  conflicts_with "geoip",
+    because: "geoip has been disabled"
 
   resource "database" do
     url "https://src.fedoraproject.org/lookaside/pkgs/GeoIP/GeoIP.dat.gz/4bc1e8280fe2db0adc3fe48663b8926e/GeoIP.dat.gz"
